2a26f3671a6e18f888db6d68e6bb1af87fd55850
JayMehtaJBM <69752445+JayMehtaJBM@users.noreply.github.com>
2020-10-09 13:16:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+function createSquare() {
+
+    const colors = [
+        '2196f3' , 'e91e63' , 'ffeb3b' , '74ff1d'
+    ]
+
+    const section = document.querySelector('section')
+    const square = document.createElement('span');
+
+    var size = Math.random() * 50;
+
+    square.style.width = 20 + size + 'px';
+    square.style.height = 20 + size + 'px';
+
+    square.style.top = Math.random() * window.innerHeight + 'px';
+    square.style.left = Math.random() * window.innerWidth + 'px';
+
+    const bg = colors[Math.floor(Math.random() * colors.length)];
+
+    square.style.background = '#' + bg;
+
+    section.appendChild(square);
+
+    
+
+    setTimeout( () => {
+        square.remove();
+    } , 3000)
+}
+
+setInterval( createSquare , 150)
